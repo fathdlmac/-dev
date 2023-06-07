c9b37185e3f7bde989d77d9f742aca34535bf04e
fathdlmac <102386734+fathdlmac@users.noreply.github.com>
2023-06-07 05:18:25 +0300

    Delete lib directory

--- lib/models/cart_item.dart
+++
@@ -1,8 +0,0 @@
-import '../models/flower.dart';
-
-class CartItem {
-  final Flower flower;
-  int quantity;
-
-  CartItem({required this.flower, this.quantity = 1});
-}
--- lib/models/flower.dart
+++
@@ -1,7 +0,0 @@
-class Flower {
-  final String name;
-  final double price;
-  final String imageUrl;
-
-  Flower({required this.name, required this.price, required this.imageUrl});
-}
